b285282a1a5a6e61532ae182606e7bc3897cda53
oleh <tajcig@ya.ru>
2017-01-03 10:52:40 +0200

    added menu

--- Classes/MainScene.cpp
+++ Classes/MainScene.cpp
@@ -3,6 +3,7 @@
 #include "GameScene.h"
 
 
+
 USING_NS_CC;
 
 Scene* MainScene::createScene()
@@ -25,10 +26,19 @@ bool MainScene::init()
     
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    auto menuTitle = MenuItemImage::create("button.png","button_pressed.png");
+
+    auto playItem = MenuItemImage::create("button.png","button_pressed.png",CC_CALLBACK_1(MainScene::goToGameScene,this));
+
+    auto menu = Menu::create(menuTitle,playItem,NULL);
+    menu->alignItemsVerticallyWithPadding(visibleSize.height/12);
+    this->addChild(menu);
+
     return true;
 }
 
 void MainScene::goToGameScene(cocos2d::Ref *pSender){
+
 	auto scene = GameScene::createScene();
 	Director::getInstance()-> replaceScene(scene);
 }
